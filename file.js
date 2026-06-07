fd182c59b5a624f4403028fab704dde71ce7e0e8
dund11 <252978918+dund11@users.noreply.github.com>
2026-06-07 20:29:54 +0700

    loop for

--- file.js
+++ file.js
@@ -1,9 +1,14 @@
 const container = document.querySelector("#container");
-
 const square = document.createElement("div");
 
-div.addEventListener("mouseover", function() {
-    div.style.backgroundColor = "black";
+container.appendChild(square);
+
+square.addEventListener("mouseover", function() {
+    square.style.backgroundColor = "black";
 });
 
 let size = prompt("Enter grid size (max 100):");
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+}
