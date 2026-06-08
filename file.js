ae139b61ddc893640f97f06a1137a177f124c751
dund11 <252978918+dund11@users.noreply.github.com>
2026-06-08 21:40:30 +0700

    loop square

--- file.js
+++ file.js
@@ -2,6 +2,16 @@ const container = document.querySelector("#container");
 
 function createGrid(size) {
     container.innerHTML = "";
+
+    const squareSize = 640 / size;
+
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        square.style.float = "left";
+        square.style.border = "1px solid #ccc";
+        container.appendChild(square);
 }
 
 createGrid(16);
