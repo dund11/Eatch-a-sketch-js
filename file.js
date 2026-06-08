c3df5d8752e7f6387be99c367c60c0b7d01383e0
dund11 <252978918+dund11@users.noreply.github.com>
2026-06-08 21:54:33 +0700

    resetbutton

--- file.js
+++ file.js
@@ -1,23 +1,37 @@
 const container = document.querySelector("#container");
+const resertButton = document.querySelector("#reset");
 
 function createGrid(size) {
     container.innerHTML = "";
-
     const squareSize = 640 / size;
 
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement("div");
-        square.style.width = squareSize + "px";
-        square.style.height = squareSize + "px";
-        square.style.float = "left";
-        square.style.border = "1px solid #ccc";
-        square.addEventListener("mouseover", function() {
+        square.classList.add("grid-square");
+
+        square.style.width = `${squareSize} px`;
+        square.style.height = `${squareSize} px`;
+
+        square.addEventListener("mouseover", () => {
         square.style.backgroundColor = "black";
 });
+
         container.appendChild(square);
+    }
 }
 
-createGrid(16);
+resertButton.addEventListener("click", () => {
+    let newSize = prompt("Enter new grid size (max 100):");
+    
+    if (isNaN(newSize) || newSize <= 0) {
+        alert("Please enter a valid number.");
+        return;
+    }
+
+    if (newSize > 100) size = 100;
+
+    createGrid(size);
+});
 
 
 
